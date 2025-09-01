cf59683e47024bb3a63b1c1eb4d03d73c8af8d0d
vega2kgkube <vega2kgkube@gmail.com>
2025-09-01 17:25:35 +0900

    App remove

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ class App extends Component {
 
   render() {
     const { todo, todos } = this.state;
-    const { handleChange, handleCreate, handleEnter, handleToggle } = this;
+    const { handleChange, handleCreate, handleEnter, handleToggle, handleRemove } = this;
 
     return (
       <TodoListTemplate form=
@@ -68,7 +68,10 @@ class App extends Component {
           myChange={handleChange}
           myCreate={handleCreate} />
         }>
-        <TodoItemList myTodos={todos} myToggle={handleToggle} />
+        <TodoItemList myTodos={todos} 
+            myToggle={handleToggle} 
+            myRemove={handleRemove}
+        />
       </TodoListTemplate>
     );
   } //render
