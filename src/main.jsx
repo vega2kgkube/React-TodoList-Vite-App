13d5848ecdf13ba698bc129cfbe3b7a73d0c9143
vega2kgkube <vega2kgkube@gmail.com>
2025-09-03 09:33:47 +0900

    configStore add

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { toDoReducer } from './reducers';
+
+//const store = createStore(toDoReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: toDoReducer,
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
