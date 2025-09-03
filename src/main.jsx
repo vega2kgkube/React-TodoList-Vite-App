a11bb19847f04aab5061506fa426893e90d88c6f
vega2kgkube <vega2kgkube@gmail.com>
2025-09-03 13:06:11 +0900

    절대경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '@/index.css'
+import App from '@/App.jsx'
 
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { toDoReducer } from './reducers';
+import { toDoReducer } from '@/reducers';
 
 //const store = createStore(toDoReducer, applyMiddleware(thunk));
 const store = configureStore({
