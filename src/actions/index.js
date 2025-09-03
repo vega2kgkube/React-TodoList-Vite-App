c784f2d3d621b42611da3fc5b0a4824404f606d4
vega2kgkube <vega2kgkube@gmail.com>
2025-09-03 10:23:12 +0900

    Action addTodo

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ const apiUrl = `${BASE_URL}/todos`;
 
 //Action type 정의
 export const FETCH_TODOS = "FETCH_TODOS";
+export const ADD_TODO = "ADD_TODO";
 
 //TodoList Action 함수
 export const fetchAllTodos = () => {
@@ -20,3 +21,20 @@ export const fetchAllTodos = () => {
             });
     }
 }; //fetchAllTodos
+
+//Todo 등록 Action 함수
+export const addTodo = (todo) => {
+    return (dispatch) => {
+        axios.post(apiUrl, todo)
+            .then(res => {
+                dispatch({
+                    type: ADD_TODO,
+                    payload: res.data
+                })
+            })
+            .catch(error => {
+                console.log(error);
+                throw (error);
+            })
+    }
+}; //addTodo
